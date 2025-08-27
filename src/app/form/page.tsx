15e657aacc36d4c53197c80de0938af760003513
f0bima <f0bima@student.uns.ac.id>
2025-08-27 15:10:48 +0700

    form handler on submit

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,40 +1,42 @@
 "use client";
-import React, { ChangeEventHandler, useState } from "react";
+
+import { FormEventHandler } from "react";
 
 const Page = () => {
-  const [firstname, setFirstname] = useState<string>();
-  const [lastname, setLastname] = useState<string>();
+  // const [firstname, setFirstname] = useState<string>();
+  // const [lastname, setLastname] = useState<string>();
 
-  const onFirstnameChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    const value = e.target.value;
-    setFirstname(value);
+  // const onFirstnameChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+  //   const value = e.target.value;
+  //   setFirstname(value);
+  // };
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const _firstname = form.get("firstname");
+    const _lastname = form.get("lastname");
+
+    console.log("submited...", { _firstname, _lastname });
   };
 
   return (
     <main className="flex w-full h-screen justify-center items-center flex-col gap-2">
-      <div className="">
-        <p>nilai :</p>
-        <p>firstname : {firstname}</p>
-        <p>lastname : {lastname}</p>
-      </div>
-      <form className="flex flex-col gap-2">
+      <form className="flex flex-col gap-2" onSubmit={onSubmit}>
         <input
           className="p-2 border rounded-lg"
           type="text"
           placeholder="Firstname..."
           name="firstname"
-          onChange={onFirstnameChange}
+          // onChange={onFirstnameChange}
         />
         <input
           className="p-2 border rounded-lg"
           type="text"
           placeholder="Lastname..."
           name="lastname"
-          onChange={(e) => {
-            const value = e.target.value;
-            setLastname(value);
-          }}
         />
+        <button type="submit">Submit</button>
       </form>
     </main>
   );
