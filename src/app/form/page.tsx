b9219659be32c1d78d927407819eb556e9d8a7d0
f0bima <f0bima@student.uns.ac.id>
2025-08-27 15:34:35 +0700

    zod validation

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { FormEventHandler } from "react";
+import { FormEventHandler, useState } from "react";
+import z from "zod";
 
 const Page = () => {
   // const [firstname, setFirstname] = useState<string>();
@@ -11,13 +12,50 @@ const Page = () => {
   //   setFirstname(value);
   // };
 
+  const [errors, setErrors] = useState<{
+    firstname?: string;
+    lastname?: string;
+    age?: string;
+  }>();
+
   const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
     const form = new FormData(e.currentTarget);
     const _firstname = form.get("firstname");
     const _lastname = form.get("lastname");
+    const _age = parseInt(form.get("age")?.toString() ?? "0");
 
-    console.log("submited...", { _firstname, _lastname });
+    const formSchema = z.object({
+      firstname: z.string().min(3).max(10),
+      lastname: z.string().min(3).max(10),
+      age: z.number(),
+    });
+
+    const formData = { firstname: _firstname, lastname: _lastname, age: _age };
+
+    console.log("Formdata", formData);
+    const validatedFormData = formSchema.safeParse(formData);
+    if (!validatedFormData.success) {
+      const issues = validatedFormData.error.issues;
+
+      const firstnameError = issues.find((issue) =>
+        issue.path.find((path) => path === "firstname")
+      );
+
+      const lastnameError = issues.find((issue) =>
+        issue.path.find((path) => path === "lastname")
+      );
+
+      setErrors({
+        firstname: firstnameError?.message,
+        lastname: lastnameError?.message,
+      });
+
+      console.log("error", firstnameError);
+
+      return;
+    }
+    console.log("validatedFormData", validatedFormData.data);
   };
 
   return (
@@ -30,12 +68,23 @@ const Page = () => {
           name="firstname"
           // onChange={onFirstnameChange}
         />
+        <p>{errors?.firstname}</p>
         <input
           className="p-2 border rounded-lg"
           type="text"
           placeholder="Lastname..."
           name="lastname"
         />
+        <p>{errors?.lastname}</p>
+
+        <input
+          className="p-2 border rounded-lg"
+          type="number"
+          placeholder="age..."
+          name="age"
+        />
+        <p>{errors?.age}</p>
+
         <button type="submit">Submit</button>
       </form>
     </main>
